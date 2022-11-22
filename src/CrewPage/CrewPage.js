5b49e83f3aa802aa15172a635ff512a0b93168c6
codeandjoy <87646754+codeandjoy@users.noreply.github.com>
2022-11-22 22:41:54 +0200

    Now fetching crew data from API

--- src/CrewPage/CrewPage.js
+++ src/CrewPage/CrewPage.js
@@ -1,18 +1,16 @@
+import { useSpace } from '../hooks/SpaceHooks';
 import CrewMember from './CrewMember';
 import './css/CrewPage.css';
 
 const CrewPage = () => {
-    const sampleData = [
-        { name: 'Skott Tingle' },
-        { name: 'Richard Amold' }
-    ]
+    const { crewData } = useSpace();
 
     return (
         <div className="crew-page">
             <p className='crew-count'>Currently 6 people on ISS</p>
  
             <div className='crew-members'>
-                {sampleData.map(crewMember => (
+                {crewData && crewData.map(crewMember => (
                     <CrewMember
                         key={ crewMember.name }
                         name={ crewMember.name }
